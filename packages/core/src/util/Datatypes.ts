0784aced83fa9fdf78ee7166fb0ee73983dd734c
sakshi-dhamija <sakshidhamija15@gmail.com>
2022-12-20 13:41:34 +0530

    Created basic structure for core and added utility datatypes

---
+++ packages/core/src/util/Datatypes.ts
@@ -0,0 +1,39 @@
+/**
+ * Hardcoded list of symbols
+ */
+ export enum Stock {
+    'RIL' = 'RIL',
+    'TSLA' = 'TSLA',
+    'AMZN' = 'AMZN',
+}
+
+export enum OperationResponseStatus {
+    'Success',
+    'Error',
+}
+
+export interface Message {
+    message: string;
+}
+
+interface OperationSuccessResponse<T> {
+    status: OperationResponseStatus.Success;
+    messages?: Message[];
+    data: T;
+}
+
+interface OperationErrorResponse {
+    status: OperationResponseStatus.Error;
+    messages: Message[];
+}
+
+export type OperationResponse<T> = OperationErrorResponse | OperationSuccessResponse<T>;
+
+export enum SortOrder {
+    'Ascending',
+    'Descending',
+}
+
+export type Quantity = number;
+export type Amount = number;
+export type ID = number;
