aa39123a1c21dda141718aeed52dc60884b6122e
sakshi-dhamija <sakshidhamija15@gmail.com>
2022-12-20 17:19:44 +0530

    updated models

---
+++ packages/api/src/models/Order.ts
@@ -0,0 +1,122 @@
+import {
+    Stock,
+    OrderType,
+    Quantity,
+    Amount,
+    User,
+    Order,
+    OrderInput,
+    OperationResponse,
+    IOrderStore,
+    OrderStatus,
+    OperationResponseStatus,
+    AdditionalOrderType,
+    ID,
+} from '../../../core';
+
+type TruncatedOrderDetails = Omit<OrderInput, 'user'> & {
+    id: ID;
+    time: Date;
+};
+
+type ConfirmedOrderDetails = TruncatedOrderDetails & {
+    status: OrderStatus.Confirmed;
+    avgSettledPrice: Amount;
+    settledTime: Date;
+};
+
+type PlacedOrderDetails = TruncatedOrderDetails & {
+    status: OrderStatus.Placed;
+};
+
+type PartiallyFilledOrderDetails = TruncatedOrderDetails & {
+    status: OrderStatus.PartiallyFilled;
+    quantityFilled: Quantity;
+};
+
+export type OrderDetails = ConfirmedOrderDetails | PlacedOrderDetails | PartiallyFilledOrderDetails;
+
+export interface OrderStoreDetails
+    extends Omit<IOrderStore, 'placedBuyOrders' | 'placedSellOrders' | 'confirmedOrders'> {
+    placedBuyOrders: OrderDetails[];
+    placedSellOrders: OrderDetails[];
+    confirmedOrders: OrderDetails[];
+}
+
+export type OrderRepositoryResponse = OperationResponse<OrderDetails>;
+
+export class OrderRepository {
+    public static placeOrder(
+        user: User,
+        symbol: Stock,
+        type: OrderType,
+        additionalOrderType: AdditionalOrderType,
+        quantity: Quantity,
+        price: Amount,
+    ): OrderRepositoryResponse {
+        const response =
+            additionalOrderType === AdditionalOrderType.Limit
+                ? user.placeOrder(symbol, type, AdditionalOrderType.Limit, quantity, price)
+                : user.placeOrder(symbol, type, AdditionalOrderType.Market, quantity);
+        if (response.status === OperationResponseStatus.Success) {
+            return {
+                status: response.status,
+                data: this.getOrderDetails(response.data),
+                messages: response.messages,
+            };
+        } else {
+            return {
+                status: response.status,
+                messages: response.messages,
+            };
+        }
+    }
+
+    public static getOrderDetails(order: Order): OrderDetails {
+        const orderInput: OrderInput = {
+            ...{
+                quantity: order.getQuantity(),
+                symbol: order.getSymbol(),
+                type: order.getOrderType(),
+                user: order.user,
+            },
+            ...(order.getAdditionalOrderType() === AdditionalOrderType.Market
+                ? { additionalType: AdditionalOrderType.Market }
+                : { additionalType: AdditionalOrderType.Limit, price: order.getPrice() }),
+        };
+        const orderDetails: TruncatedOrderDetails = {
+            ...{ ...orderInput, user: undefined },
+            id: order.getId(),
+            time: order.getTime(),
+        };
+        if (order.getStatus() === OrderStatus.Confirmed) {
+            return {
+                ...orderDetails,
+                status: OrderStatus.Confirmed,
+                avgSettledPrice: order.getAvgSettledPrice(),
+                settledTime: order.getSettledTime(),
+            };
+        } else if (order.getStatus() === OrderStatus.PartiallyFilled) {
+            return {
+                ...orderDetails,
+                status: OrderStatus.PartiallyFilled,
+                quantityFilled: order.getQuantitySettled(),
+            };
+        } else if (order.getStatus() === OrderStatus.Placed) {
+            return {
+                ...orderDetails,
+                status: OrderStatus.Placed,
+            };
+        } else {
+            throw new Error('Invalid Order Status');
+        }
+    }
+
+    public static getOrderStoreDetails(orderStore: IOrderStore): OrderStoreDetails {
+        return {
+            placedBuyOrders: orderStore.placedBuyOrders.map((order) => this.getOrderDetails(order)),
+            placedSellOrders: orderStore.placedSellOrders.map((order) => this.getOrderDetails(order)),
+            confirmedOrders: orderStore.confirmedOrders.map((order) => this.getOrderDetails(order)),
+        };
+    }
+}
---
+++ packages/api/src/models/User.ts
@@ -0,0 +1,99 @@
+import { User, Amount, HoldingsData, IUser } from '../../../core';
+import * as bcrypt from 'bcrypt';
+import { Socket } from 'socket.io';
+import { OrderRepository, OrderStoreDetails } from './Order';
+
+const saltRounds = 10;
+
+export interface WalletDetails {
+    margin: Amount;
+}
+
+export interface UserDetails extends Omit<IUser, 'orders' | 'holdings' | 'name' | 'wallet'> {
+    orders: OrderStoreDetails;
+    holdings: HoldingsData;
+    wallet: WalletDetails;
+}
+
+export interface UserStoreItem {
+    user: User;
+    username: string;
+    socket?: Socket;
+}
+
+export interface UserStoreItemDetails extends UserDetails {
+    username: string;
+}
+
+export interface UserStoreItemSensitive extends UserStoreItem {
+    password: string;
+}
+export class UserStore {
+    private static users: Map<string, UserStoreItemSensitive> = new Map<string, UserStoreItemSensitive>();
+
+    public static async addUser(
+        username: string,
+        password: string,
+        balance: Amount,
+        holdings: HoldingsData,
+    ): Promise<UserStoreItem> {
+        if (this.users.get(username)) {
+            throw new Error('Username already exists');
+        }
+        const userStoreItem: UserStoreItemSensitive = {
+            user: new User(username, balance, holdings),
+            password: await bcrypt.hash(password, saltRounds),
+            username: username,
+        };
+        this.users.set(username, userStoreItem);
+        return this.findUserByUsername(username);
+    }
+
+    public static findUserByUsername(username: string): UserStoreItem {
+        const user = this.users.get(username);
+        if (user) {
+            return {
+                user: user.user,
+                username: user.username,
+                socket: user.socket,
+            };
+        }
+        throw new Error('User not found');
+    }
+
+    public static async findUserByUsernameAndAuthenticate(username: string, password: string): Promise<UserStoreItem> {
+        const user = this.users.get(username);
+        if (!user) {
+            throw new Error('User not found');
+        } else if (await bcrypt.compare(password, user.password)) {
+            return this.findUserByUsername(username);
+        } else {
+            throw new Error('Wrong password');
+        }
+    }
+
+    public static getUserDetails(user: User): UserDetails {
+        return {
+            id: user.getId(),
+            wallet: { margin: user.wallet.getMargin() },
+            orders: OrderRepository.getOrderStoreDetails(user.orders.getData()),
+            holdings: user.holdings.getHoldings(),
+        };
+    }
+
+    public static getUserStoreItemDetails(user: UserStoreItem): UserStoreItemDetails {
+        return {
+            ...this.getUserDetails(user.user),
+            username: user.username,
+        };
+    }
+
+    public static setSocketForUser(username: string, socket: Socket): void {
+        const user = this.users.get(username);
+        if (user) {
+            user.socket = socket;
+        } else {
+            throw new Error('User not found');
+        }
+    }
+}
